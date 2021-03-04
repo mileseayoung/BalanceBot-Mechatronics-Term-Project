48a397ff0c91ec72d5cb0fa0025f523fdc0221ef
MilesYoung <myoung39@calpoly.edu>
2021-03-04 10:36:53 -0800

    More documentation updates and also fixing the issue with initializing the fault pin

--- Lab08/Motor_Driver.py
+++ Lab08/Motor_Driver.py
@@ -1,8 +1,15 @@
-''' @file Motor_Driver.py
-@brief this driver controls the acuation of the motor
-@details This class allows modules to setup a motor on avaliable pins and then send 
-input duty cycles to it which will then use PWM to move the motor
-Source Code: https://bitbucket.org/CraigKimball/me305_labs/src/master/Lab06/Motor_Driver.py
+''' @file       Motor_Driver.py
+    @brief      <b>Motor Driver Class</b>
+    @details    This class allows modules to set up a motor on avaliable pins 
+                and then send input duty cycles to it which will then use PWM 
+                to control motor movement. During initialization, the user must 
+                define the desired pins, timer, and timer channels used to control 
+                the motor. These are then utilized in methods that enable or disable 
+                the motor or set the PWM duty cycle to the motor. An external 
+                interrupt on the fault pin of the motor immediately disables the 
+                motor if a fault is detected and prompts the user to signal for 
+                the motor to resume functioning.\n
+                <em>Source Code:</em> https://bitbucket.org/CraigKimball/me305_labs/src/master/Lab06/Motor_Driver.py
  '''
 import pyb
 
@@ -11,20 +18,22 @@ class MotorDriver:
  ME405 board. '''
 
  def __init__ (self, motorNum, nSLEEP_pin, nFAULT_pin, IN1_pin, channel1, IN2_pin, channel2, timer):
-    ''' Creates a motor driver by initializing GPIO
-     pins and turning the motor off for safety.
-     @param nSLEEP_pin A pyb.Pin object to use as the enable pin.
-     @param nFAULT_pin A pyb.Pin object to detect faults and trigger an external interrupt. It must be configured like this: pyb.Pin(pyb.Pin.cpu.[pinNum])
-     @param IN1_pin A pyb.Pin object to use as the input to half bridge 1.
-     @param channel1 A number identifier for the timer channel for pinIN1.
-     @param IN2_pin A pyb.Pin object to use as the input to half bridge 2.
-     @param channel2 A number identifier for the timer channel for pinIN2
-     @param timer A pyb.Timer object to use for PWM generation on
-     IN1_pin and IN2_pin. 
+    ''' 
+    @brief              <b>Motor Driver Constructor</b> 
+    @details            Creates a motor driver by initializing GPIO
+                        pins and turning the motor off for safety.
+     @param nSLEEP_pin  A pyb.Pin object to use as the enable pin.
+     @param nFAULT_pin  A pyb.Pin object to detect faults and trigger an external 
+                        interrupt. It must be configured like this: pyb.Pin(pyb.Pin.cpu.[pinNum])
+     @param IN1_pin     A pyb.Pin object to use as the input to half bridge 1.
+     @param channel1    A number identifier for the timer channel for pinIN1.
+     @param IN2_pin     A pyb.Pin object to use as the input to half bridge 2.
+     @param channel2    A number identifier for the timer channel for pinIN2
+     @param timer       A pyb.Timer object to use for PWM generation on IN1_pin and IN2_pin. 
      '''
     
     ## Identifier for the motor object from constructor
-    self.motorNum =     motorNum
+    self.motorNum   =   motorNum
     
     ## Defines local object for enable pin from constructor
     self.nSleep_pin =   nSLEEP_pin
@@ -37,9 +46,11 @@ class MotorDriver:
     
     ## Defines local timer that will be used from constructor
     self.timer      =   timer
-   
+    
+    ## Defines local object for fault pin from constructor
+    self.nFault_pin =   nFAULT_pin
     # Initialize fault pin to function with external interrupt method
-    self.nFault_Pin = nFAULT_pin.init(pyb.ExtInt.IRQ_FALLING,pyb.Pin.PULL_UP,faultInterrupt)
+    self.nFault_pin.init(pyb.ExtInt.IRQ_FALLING,pyb.Pin.PULL_UP,faultInterrupt)
     
     ## setting up the channels for PWM on the motors
     self.timch2 = self.timer.channel(channel2,pyb.Timer.PWM,pin = self.IN2_pin)
@@ -48,12 +59,21 @@ class MotorDriver:
 
 ## Sets the sleep pin of the motor to high
  def enable (self):
+     '''
+     @brief     <b>Enable Motor Movement</b>
+     @details   Sets the sleep pin of the motor to high, enabling the motor to move. 
+                This method must be called before setting the duty cycle of the motor.
+     '''
      #print ('Enabling Motor')
      self.nSleep_pin.high()
      self.t3ch2.pulse_width_percent(0)
      self.t3ch1.pulse_width_percent(0)
 ## sets the sleep pin of the motor to low
  def disable (self):
+     '''
+     @brief     <b>Disable Motor Movement</b>
+     
+     '''
      print ('Disabling Motor')
      self.nSleep_pin.low()
      
@@ -69,6 +89,7 @@ class MotorDriver:
          self.timch2.pulse_width_percent(0)
          self.timch1.pulse_width_percent(100)
      
+    # Clamps the duty cycle between the meaningful values of 0% and 100%. Non meaningful values outside these bounds are moved within them before setting duty cycle.
      if(duty >= 0):
          self.timch2.pulse_width_percent(duty)
          self.timch1.pulse_width_percent(0)
@@ -88,7 +109,8 @@ class MotorDriver:
 
 def faultInterrupt(self,fault_pin):
     '''
-    External interrupt method which is triggered when the motor H-bridge fault pin goes low.
+    @brief      ...
+    @Details    External interrupt method which is triggered when the motor H-bridge fault pin goes low.
     '''
     
     # Immediately disable the motor
